9a11ccf2ca5825f6dea1aff1fa357f2de6d2df48
RRFIM <saswswap@gmail.com>
2025-10-14 16:33:30 +0200

    First part done

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
-
-
-function main(){
-    console.log("Hello meteo !");
-
+function main() {
+    Onstill()
+    
 }
 main();
 
+function Onstill()
+{
+    const Lati_elem = document.body.querySelector(".Lati")
+    const Long_elem = document.body.querySelector(".Long")
 
+    const MainPosition = navigator.geolocation.getCurrentPosition(
 
-/**
- * S'execute quand l'utilisateur a accepté la geolocalisation.
- * 
- * @param {*} position_obj 
- */
-function onPosition(position_obj){
-
+        (PositionCurent_obj) => {
+            const MainLatitude = PositionCurent_obj.coords.latitude
+            const MainLong = PositionCurent_obj.coords.longitude
+    
+            Temperature(MainLatitude,MainLong)
+        }
+    )
 }
+
+
+async function Temperature(lat,long) 
+{
+    const meteo_obj = await fetch("https://api.open-meteo.com/v1/forecast?latitude="+(lat)+"&longitude="+(long)+"&current=temperature_2m").then(res => res.json())
+    const MainTemperature = meteo_obj.current.temperature_2m
+}
+
